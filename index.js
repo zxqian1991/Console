92d711347b1e0878a009f30f769dc63d41a35392
Bharathvaj <bharathvaj1995@gmail.com>
2017-10-23 12:12:56 +0530

    Complete es6 code refractor

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-var Console = {
-	warn: log(3), // 黄色
-	error: log(1),  // 红色
-	log: log(7), // 白色
-	debug: log(6), // 青色 
-	success: log(2), // 绿色
-	stress: log(5) // 洋红色,
+const consoleMessageTypes = [
+    '\x1b[30m',
+    '\x1b[31m',
+    '\x1b[32m',
+    '\x1b[33m',
+    '\x1b[34m',
+    '\x1b[35m',
+    '\x1b[36m',
+    '\x1b[37m'
+];
+const log = type => {
+    return (...args) => {
+        let outputArg = '';
+        const len = args.length;
+        for (let i in args) {
+            if (i == 0) {
+                outputArg += args[i];
+            } else {
+                outputArg += ' ' + args[i];
+            }
+        }
+        outputArg = consoleMessageTypes[type] + outputArg + consoleMessageTypes[7];
+        console.log(outputArg);
+    };
 };
-var lists =  ["\x1b[30m","\x1b[31m","\x1b[32m","\x1b[33m","\x1b[34m","\x1b[35m","\x1b[36m","\x1b[37m"];
-function log(type) {
-	return function(){
-		var arg = "";
-		var len = arguments.length;
-		for(var i in arguments) {
-			if(i == 0) {
-				arg += arguments[i]
-			} else {
-				arg += " " + arguments[i];
-			}
-		}
-		arg = lists[type] + arg + lists[7];
-		// eval(arg);
-		console.log(arg)
-	}
-}
-module.exports = Console;
+module.exports = {
+    warn: log(3), // 黄色
+    error: log(1), // 红色
+    log: log(7), // 白色
+    debug: log(6), // 青色
+    success: log(2), // 绿色
+    stress: log(5) // 洋红色,
+};
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-var $console = require("./index");
-a = "qianzhixiang";
-b = "jianghuihui"
-$console.log(a,b)
-$console.error(a,b)
+var $console = require('./index');
+a = 'qianzhixiang';
+b = 'jianghuihui';
+$console.log(a, b);
+$console.log('This is a LOG message');
+$console.debug('This is a DEBUG message');
+$console.error('This is a ERROR message');
+$console.warn('This is a WARN message');
+$console.stress('This is a STRESS message');
+$console.success('This is a SUCCESS message');
